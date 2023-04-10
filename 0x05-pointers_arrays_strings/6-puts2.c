a61646befb080eb384d5abd04d001e11835fb486
gibsonngulube <gibsonngulube100@gmail.com>
2023-04-10 06:11:02 +0000

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main"
+/**
+  *puts2 - prints evert other
+  *character of a string, starting
+  *with the 1st character
+  *@s: a pointer parameter
+  *Return: nothing
+  */
+void puts2(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(s[i]);
+		}
+		i++;
+	}
+}
