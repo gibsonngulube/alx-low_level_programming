55214916e74a25f1d377ea82f6a4c1047ec2193a
gibsonngulube <gibsonngulube100@gmail.com>
2023-04-10 07:07:39 +0000

    print 1/2 a str

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  *puts_half - prints the 2nd 1/2 of a string
+  *@str: points to a string parameter
+  *
+  *Return: nothing
+  */
+void puts_half(char *str)
+{
+	int len = 0;
+
+	int i 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	i = (len + 1) / 2;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
