05afbe86b623efbed6850fc51c43bc986561aaa5
gibsonngulube <gibsonngulube100@gmail.com>
2023-04-11 13:16:09 +0000

    print elements of an int array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,16 +10,10 @@
   **/
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
-	while (i <= (n - 1))
+	for (i = 0; i < n; i++)
 	{
-		printf("%d", *(a + i));
-
-		if (i <= (n - 2))
-		{
-			printf(", ");
-		}
-		i++;
+		printf("%d, ", *(a + i));
 	}
 }
