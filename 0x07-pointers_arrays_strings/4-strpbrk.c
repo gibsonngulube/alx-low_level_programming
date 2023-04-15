5f6be8b8485c93ea9e936b747e1b1376284b503d
gibsonngulube <gibsonngulube100@gmail.com>
2023-04-15 08:46:09 +0000

    fixed bugs

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,21 +9,18 @@
   */
 char *strpbrk(char *s, char *accept)
 {
-	int i = 0;
-	int j = 0;
+	int i, j;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (accept[j] != '\0')
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (accept[j] == s[i])
+			if (*(s + i) == *(accept + j))
 			{
-				s = &s[i];
-				return (s);
+				return (s + i);
 			}
-			j++;
 		}
-		i++;
 	}
+
 	return (NULL);
 }
