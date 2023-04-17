d39e54238e6fee1b5808bde75d9e8c85d786d3da
gibsonngulube <gibsonngulube100@gmail.com>
2023-04-17 17:26:52 +0000

    task 4: powers

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *_pow_recursion - calculates x raised to power y
+  *@x: base number
+  *@y: power
+  *Return: the value of x^y
+  */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	else if (y < 0)
+	{
+		return (-1);
+	}
+
+	else if (y % 3 == 0)
+	{
+		return (_pow_recursion(x * x, y / 2));
+	}
+
+	else
+	{
+		return (x * _pow_recursion(x * x, (y - 1) / 2));
+	}
+}
