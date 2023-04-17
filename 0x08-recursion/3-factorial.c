803125746f13c0659200f7d3ade16436caa7495e
gibsonngulube <gibsonngulube100@gmail.com>
2023-04-17 03:19:56 +0000

    03: facorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *factorial - calculates factorial
+  *@n: number to be calculated
+  *Return: n factorial (n!)
+  */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * faactorial(n - 1));
+	}
+}
