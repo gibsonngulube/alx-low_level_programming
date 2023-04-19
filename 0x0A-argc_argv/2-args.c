1256641c1fbc80673421c4eacc8fde8472cb0535
gibsonngulube <gibsonngulube100@gmail.com>
2023-04-19 03:33:56 +0000

    2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *main - Entry point
+  *@argc: counter
+  *@argv: string array
+  *Return: Always 0
+  */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
