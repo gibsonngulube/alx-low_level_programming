f20eb511038d4d8fb5eb8657b4391d7a4b6500b2
gibsonngulube <gibsonngulube100@gmail.com>
2023-04-19 02:56:24 +0000

    task 0

---
+++ 0-whatsmyname.c/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *main - Entry point
+  *@argc: counter
+  *@argv: str array
+  *Return: Always 0
+  */
+int main(int argc, char *argv[])
+{
+	printf("$s\n", argv[0]);
+
+	return (0);
+}
